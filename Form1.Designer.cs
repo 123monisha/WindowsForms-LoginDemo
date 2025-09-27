5f1e19dd6f3461fe88a132b54b3a77d3d29cf972
Monisha Ishwar <monishamoger15@gmail.com>
2025-09-27 19:20:47 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Windows_Login_Registration_Foem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPwd = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(247, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login Page";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(130, 139);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(73, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "UserName";
+            // 
+            // lblPwd
+            // 
+            this.lblPwd.AutoSize = true;
+            this.lblPwd.Location = new System.Drawing.Point(130, 207);
+            this.lblPwd.Name = "lblPwd";
+            this.lblPwd.Size = new System.Drawing.Size(67, 16);
+            this.lblPwd.TabIndex = 2;
+            this.lblPwd.Text = "Password";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(250, 139);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(278, 22);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(250, 207);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(278, 22);
+            this.txtPwd.TabIndex = 4;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(250, 274);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(453, 274);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(249, 327);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 16);
+            this.lblMsg.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 392);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblPwd);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPwd;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Login_Registration_Foem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection();
+
+        public Form1()
+        {
+            InitializeComponent();
+            con.ConnectionString= @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Banking; Integrated Security=true";
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            cmd.CommandText = "select Accno, Name,Address from Customers where Name=@name";
+            cmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+            SqlDataReader dr = cmd.ExecuteReader();
+            if(dr.HasRows)
+            {
+                dr.Read();
+                if (dr[2].ToString().Equals(txtPwd.Text.Trim()))
+                {
+                    lblMsg.Text = "you are successfully logged in";
+                }
+                else
+                {
+                    lblMsg.Text = "pasword is incorrect";
+                }
+            }
+            else
+            {
+                lblMsg.Text = "User not found! First do a REgistration";
+            }
+            dr.Close();
+            con.Close();
+        }
+
+        private void lnkReg_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+      
+        {
+        }
+    }
+}
